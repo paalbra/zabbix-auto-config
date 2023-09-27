b7f2a3d01690cb2bca287f30757e42d884ca5f8e
Paal Braathen <paalbra@gmail.com>
2023-09-27 14:52:51 +0200

    Make test use tomllib

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
 import os
 import unittest
-import tomli
+import tomllib
 
 import pytest
 from pydantic.error_wrappers import ValidationError
@@ -9,11 +9,11 @@ import zabbix_auto_config.models as models
 
 
 def test_sample_config(sample_config):
-    models.Settings(**tomli.loads(sample_config))
+    models.Settings(**tomllib.loads(sample_config))
 
 
 def test_invalid_config(sample_config):
-    config = tomli.loads(sample_config)
+    config = tomllib.loads(sample_config)
     config["foo"] = "bar"
     with pytest.raises(ValidationError) as exc_info:
         models.Settings(**config)
